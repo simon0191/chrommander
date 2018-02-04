bbca4570f2cc3a31ba2d88b1766d8edb285e535f
Simon Soriano <simon0191@gmail.com>
2018-02-04 17:08:33 +0100

    Setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const path = require('path')
 
-module.exports = [
-  {
-    name: 'background',
-    entry: './src/background/main.js',
+function fromSharedConfig(moduleName) {
+  return {
+    name: moduleName,
+    entry: `./src/${moduleName}/index.ts`,
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist', 'background')
-    }
-  },
-  {
-    name: 'foreground',
-    entry: './src/foreground/main.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist', 'foreground')
-    }
-  },
-  {
-    name: 'popup',
-    entry: './src/popup/main.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist', 'popup')
+      path: path.resolve(__dirname, 'dist', moduleName)
     }
   }
+}
+
+module.exports = [
+  fromSharedConfig('background'),
+  fromSharedConfig('foreground'),
+  fromSharedConfig('popup')
 ]
